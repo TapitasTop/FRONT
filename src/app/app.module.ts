3ba759e22e429d51b3b5dead4ab2903c82c9987a
unknown <luciaagueda.valbuena.garcia@alumnos.upm.es>
2023-03-03 15:48:45 +0100

    Creada componente de ejemplo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PaginaInicioComponent } from './pages/pagina-inicio/pagina-inicio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PaginaInicioComponent
   ],
   imports: [
     BrowserModule,
